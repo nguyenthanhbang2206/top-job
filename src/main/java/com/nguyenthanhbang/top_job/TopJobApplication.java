5e27cbbf404a2e44522834fe5f53a017a6f36713
Nguyen Thanh Bang <nguyenthanhbang2206@gmail.com>
2025-04-16 23:04:59 +0700

    init project

---
+++ src/main/java/com/nguyenthanhbang/top_job/TopJobApplication.java
@@ -0,0 +1,13 @@
+package com.nguyenthanhbang.top_job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nguyenthanhbang/top_job/TopJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyenthanhbang.top_job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
