fec1eba285f8e49968e4a0d7f24e281941e091df
Nguyen Thanh Bang <nguyenthanhbang2206@gmail.com>
2025-05-27 21:21:14 +0700

    handle exception

--- src/main/java/com/nguyenthanhbang/top_job/exception/GlobalExceptionHandler.java
+++ src/main/java/com/nguyenthanhbang/top_job/exception/GlobalExceptionHandler.java
@@ -1,8 +1,63 @@
 package com.nguyenthanhbang.top_job.exception;
 
+import com.nguyenthanhbang.top_job.dto.response.ErrorDetails;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.time.Instant;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity<ErrorDetails> handleValidationException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        ErrorDetails errorDetails = ErrorDetails.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .path(request.getRequestURI())
+                .timestamp(Instant.now())
+                .message(e.getFieldError().getDefaultMessage())
+                .error("Bad request")
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDetails);
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleException(Exception e, HttpServletRequest request) {
+        ErrorDetails errorDetails = ErrorDetails.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .path(request.getRequestURI())
+                .timestamp(Instant.now())
+                .message(e.getMessage())
+                .error("Internal server error")
+                .build();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDetails);
+    }
+    @ExceptionHandler(value = {IllegalArgumentException.class, HttpMessageNotReadableException.class})
+    public ResponseEntity<ErrorDetails> handleBadRequest(Exception e, HttpServletRequest request) {
+        ErrorDetails errorDetails = ErrorDetails.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .path(request.getRequestURI())
+                .timestamp(Instant.now())
+                .message(e.getMessage())
+                .error("Bad request")
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDetails);
+    }
+    @ExceptionHandler(value = {EntityNotFoundException.class, UsernameNotFoundException.class, NoResourceFoundException.class})
+    public ResponseEntity<ErrorDetails> handleNotFoundException(Exception e, HttpServletRequest request) {
+        ErrorDetails errorDetails = ErrorDetails.builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .path(request.getRequestURI())
+                .timestamp(Instant.now())
+                .message(e.getMessage())
+                .error("Not found")
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDetails);
+    }
 }
